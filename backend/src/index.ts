61a8e494f97a22d542fe9450f9eba566177e62c4
suhel-24 <21pa1a05g3@vishnu.edu.in>
2024-03-13 23:59:17 +0530

    done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,15 @@
 import { Hono } from 'hono'
+import { userRouter } from './routes/user';
+import { bookRouter } from './routes/blog';
 
-const app = new Hono()
+export const app = new Hono<{
+  Bindings: {
+      DATABASE_URL: string;
+      JWT_SECRET: string;
+  }
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route('/api/v1/user', userRouter)
+app.route('/api/v1/book', bookRouter)
 
 export default app
