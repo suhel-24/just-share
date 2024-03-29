f60814d695492061aff84deb18dfe4e9adb0153a
suhel-24 <21pa1a05g3@vishnu.edu.in>
2024-03-30 04:11:36 +0530

    frontend done2

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -9,7 +9,7 @@ export const Appbar = () => {
 
     return <div className="border-b flex justify-between px-10 py-4">
         <Link to={'/blogs'} className="flex flex-col justify-center cursor-pointer">
-                Medium
+                Justshare
         </Link>
         <div>
             <Link to={`/publish`}>
