953167633458ddc721d5577dd8cbe410b4eefffb
suhel-24 <21pa1a05g3@vishnu.edu.in>
2024-03-30 04:50:53 +0530

    small bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-        <Route path="/" element={<Navigate replace to="/blogs" />} />
+        <Route path="/" element={<Navigate replace to="/signin" />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
